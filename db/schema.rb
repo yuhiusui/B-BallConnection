1d3128166790a9496551fe83d1f6eb32f4904cdd
yuhiusui <usui.hanshin@gmail.com>
2020-06-05 13:43:42 +0000

    [add] controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_052156) do
+ActiveRecord::Schema.define(version: 2020_06_05_133023) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,24 @@ ActiveRecord::Schema.define(version: 2020_06_05_052156) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "courts", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "number", default: 1, null: false
+    t.string "floor", null: false
+    t.string "fee", default: "無料", null: false
+    t.string "available_time", default: "特になし", null: false
+    t.string "station"
+    t.string "court_image_id"
+    t.integer "postal_code", null: false
+    t.integer "prefecture_code", null: false
+    t.string "city", null: false
+    t.string "street", null: false
+    t.text "other"
+    t.boolean "is_valid", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "players", force: :cascade do |t|
