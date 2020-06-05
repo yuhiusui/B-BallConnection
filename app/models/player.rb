718407d9e07247a7bff887207cf7cf20148aac74
yuhiusui <usui.hanshin@gmail.com>
2020-06-05 14:40:39 +0000

    [add]route

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,13 @@ class Player < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  # 会員ステータスがtrueでないとログインできない
+  def active_for_authentication?
+    super && (self.is_valid == true)
+  end
+
+  # 会員一覧をIDの古い順番で表示
+  default_scope -> { order(created_at: :desc) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :players
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :players, path: "public/players", controllers:{
+    registrations: 'players/registrations',
+    sessions: 'players/sessions',
+    passwords: 'players/passwords'
+}
+
+  devise_for :admins, controllers:{
+    registrations: 'admins/registrations',
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords'
+}
+
+  namespace :public do
+    get '' => 'players#show'
+    get 'edit' => 'players#edit'
+    patch '' => 'players#update'
+    patch 'leave' => 'players#update_status'
+    get 'leave' => 'players#leave'
+    resources :players
+    resources :courts
+    resources :comments
+    resources :favorites
+    resources :relations
+  end
+  root 'public/homes#top'
 end
