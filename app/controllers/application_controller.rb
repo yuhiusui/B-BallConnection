2dffb81d0a454ab8de73b3ca9d847f4dd1b5ef1b
yuhiusui <usui.hanshin@gmail.com>
2020-06-06 14:49:06 +0000

    signup&in css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,23 +8,13 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-  # def after_sign_in_path_for(resource)
-  #   case resource
-  #   when Admin
-  #     admin_top_path
-  #   when Member
-  #     root_path
-  #   end
-  # end
+  def after_sign_in_path_for(resource)
+    player_path(current_player)
+  end
 
-  # def after_sign_out_path_for(resource)
-  #   case resource
-  #   when :admin
-  #     new_admin_session_path
-  #   when :member
-  #     root_path
-  #   end
-  # end
+  def after_sign_out_path_for(resource)
+    new_player_session_path
+  end
 
   def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :intro, :player_image_id,
